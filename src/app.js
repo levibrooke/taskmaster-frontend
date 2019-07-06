64d312e5c0672e4e02b80d4ae2201580ca798c5a
Levi Porter <levi@levibrooke.com>
2019-07-05 23:18:34 -0700

    Modify markup of tasks, add css

--- src/app.js
+++ src/app.js
@@ -14,7 +14,6 @@ function App() {
     })
       .then(response => response.json())
       .then(data => {
-        console.log('data', data);
         setTasks(data);
       })
       .catch(err => console.error(err));
--- src/task.js
+++ src/task.js
@@ -3,9 +3,12 @@ import React from 'react';
 const Task = (props) => (
   <li key={props.data.id}>
     <h2>{props.data.title}</h2>
-    <p>{props.data.description}</p>
-    <p>{props.data.status}</p>
-    <p>{props.data.assignee}</p>
+    <p>Description: {props.data.description}</p>
+    <p>Status: {props.data.status}</p>
+    {props.data.assignee ? 
+      <p>Assigned To: {props.data.assignee}</p> :
+      null
+    }
   </li>
 )
 
